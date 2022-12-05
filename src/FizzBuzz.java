be1fefe9b7a65a58bbaf3c5e3e03038b5f3e3af7
Akira_Matsushima <119549568+Akira-Matsushima-Yell@users.noreply.github.com>
2022-12-05 22:31:06 +0900

    作成

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,24 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		
+		for(int number=1; number<=100; number++) {
+			String numberstring =String.valueOf(number);
+			if(number%3==0 && number%5==0) {
+				numberstring ="FizzBuzz";
+				System.out.println(numberstring);
+			}else if(number%3==0 && number%5!=0) {
+				numberstring ="Fizz";
+				System.out.println(numberstring);
+			}else if(number%3!=0 && number%5==0) {
+				numberstring ="Buzz";
+				System.out.println(numberstring);
+			}else {
+				System.out.println(number);
+			}
+			
+		}
+		
 	}
 
 }
